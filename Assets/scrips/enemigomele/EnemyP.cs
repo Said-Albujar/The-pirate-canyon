6e5cfe729224966681ddf4262da7edaeb08760e3
Said Albujar Torres <saidalbujar23@gmail.com>
2022-07-06 18:42:52 -0500

    enmigo Cs

--- Assets/scrips/enemigomele/EnemyP.cs
+++ Assets/scrips/enemigomele/EnemyP.cs
@@ -10,10 +10,11 @@ public class EnemyP : MonoBehaviour
     public Quaternion angulo;
     public float grado;
 
-
+    public GameObject target;
     void Start()
     {
         ani = GetComponent<Animator>();
+        target = GameObject.Find("perso");
     }
 
     
@@ -24,28 +25,44 @@ public class EnemyP : MonoBehaviour
 
     public void Comportamiento_Enemigo()
     {
-        cronometro += 1 * Time.deltaTime;
-        if(cronometro >= 4)
+        if (Vector3.Distance(transform.position, target.transform.position) > 5)
         {
-            rutina = Random.Range(0, 2);
-            cronometro = 0;
-        }
-        switch (rutina)
-        {
-            case 0:
-                ani.SetBool("walk", false);
-                break;
+            ani.SetBool("run", false);
+            cronometro += 1 * Time.deltaTime;
+            if (cronometro >= 4)
+            {
+                rutina = Random.Range(0, 2);
+                cronometro = 0;
+            }
+            switch (rutina)
+            {
+                case 0:
+                    ani.SetBool("walk", false);
+                    break;
 
-            case 1:
-                grado = Random.Range(0, 360);
-                angulo = Quaternion.Euler(0, grado, 0);
-                rutina++;
-                break;
-            case 2:
-                transform.rotation = Quaternion.RotateTowards(transform.rotation, angulo, 0.5f);
-                transform.Translate(Vector3.forward * 1 * Time.deltaTime);
-                ani.SetBool("walk",true);
-                break;
+                case 1:
+                    grado = Random.Range(0, 360);
+                    angulo = Quaternion.Euler(0, grado, 0);
+                    rutina++;
+                    break;
+                case 2:
+                    transform.rotation = Quaternion.RotateTowards(transform.rotation, angulo, 0.5f);
+                    transform.Translate(Vector3.forward * 1 * Time.deltaTime);
+                    ani.SetBool("walk", true);
+                    break;
+            }
+        }
+
+        else
+        {
+            var lookPos = target.transform.position - transform.position;
+            lookPos.y = 0;
+            var rotation = Quaternion.LookRotation(lookPos);
+            transform.rotation = Quaternion.RotateTowards(transform.rotation, rotation, 2);
+            ani.SetBool("walk",false);
+
+            ani.SetBool("walk", true);
+            transform.Translate(Vector3.forward * 2 * Time.deltaTime);
         }
     }
 }
