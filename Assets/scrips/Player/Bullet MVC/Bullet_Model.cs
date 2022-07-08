27b559f78e2a23eb6d5a4ca471ff234ecba4f462
Said Albujar Torres <saidalbujar23@gmail.com>
2022-07-07 19:09:24 -0500

    MVC bullet

---
+++ Assets/scrips/Player/Bullet MVC/Bullet_Model.cs
@@ -0,0 +1,17 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Bullet_Model : MonoBehaviour
+{
+    public Transform spawnPoint;
+
+    public GameObject bullet;
+
+    public float shootForce = 1500f;
+    public float shootRate = 0.5f;
+
+    private float shootRateTime = 0;
+
+   
+}
